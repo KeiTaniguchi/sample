9130bd2c60532a0c44c7eb34f0d46e4b8a16bb79
KeiTaniguchi <contrabassistkei1121@gmail.com>
2015-06-07 20:35:52 +0900

    mainhikisu.cs

---
+++ 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Class1
+{
+    static void Main()
+    {
+        Console.WriteLine("みなさまはじめまして");
+        int a = 500;
+        int b = 50;
+
+        a = a + b;
+
+        Console.WriteLine("a");
+    }
+
+}
+
---
+++ mainhikisu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace hikisu
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Hello {0}さん。", args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Hello gestさん。");
+            }
+        }
+    }
+}
