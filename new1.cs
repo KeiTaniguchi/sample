4bf0926c1e29cb208aab4133b6c539d320290482
KeiTaniguchi <contrabassistkei1121@gmail.com>
2015-06-08 22:06:39 +0900

    aaaa

---
+++ new1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyObject obj = new MyObject();
+            obj.name = "谷口";
+            obj.age = 30;
+            obj.printData();
+            Console.ReadKey();
+
+        }
+    }
+
+    class MyObject
+    {
+        public string name = "(noname)";
+        public int age = 0;
+
+        public void printData()
+    {
+        Console.WriteLine("名前:{0},年齢:{1}歳" , name , age);
+    }
+    }
+}
