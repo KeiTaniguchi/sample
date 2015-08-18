57407baa5fa8b8d04505f72c59b37af1b6985ed4
taniguchi <taniguchi@core.co.jp>
2015-08-18 20:34:45 +0900

    ラムダ式、ファイル入出力

--- Lambda1/Lambda1/Form1.Designer.cs
+++ Lambda1/Lambda1/Form1.Designer.cs
@@ -31,16 +31,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 50);
+            this.label1.Location = new System.Drawing.Point(94, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 12);
             this.label1.TabIndex = 0;
@@ -49,7 +48,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 83);
+            this.label2.Location = new System.Drawing.Point(94, 96);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 12);
             this.label2.TabIndex = 1;
@@ -58,42 +57,15 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(32, 113);
+            this.label3.Location = new System.Drawing.Point(94, 126);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 12);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(155, 50);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 12);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(155, 83);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 12);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "label5";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(155, 113);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 12);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "label6";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(72, 12);
+            this.button1.Location = new System.Drawing.Point(30, 316);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -101,15 +73,32 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(200, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 201);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(224, 92);
+            this.textBox1.TabIndex = 8;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(233, 193);
+            this.ClientSize = new System.Drawing.Size(443, 446);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -125,10 +114,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Lambda1/Lambda1/Form1.cs
+++ Lambda1/Lambda1/Form1.cs
@@ -1,50 +1,96 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
 
 namespace Lambda1
 {
-
     //ラムダ式を使ってボタンクリック時に各ラベルに数字が表示されるようなものを作ってみる。
     public partial class Form1 : Form
     {
-        class kansuA
+        Func<int , string> AA = (int y) =>
         {
-            readonly Func<int, string> afunc;
+            y = 67890;
+            string GM = "おはよう"+y;
+            return GM;
+        };
 
-        }
+        //Func<string>型の AAを定義し
+        //()内の引数(int型のy)を元に
+        //=>{}内に記載されているメソッドを実行している。
+
+        Func<int , string> BB = (int x) =>
+        {
+            string GE = "こんばんは"+x;
+            return GE;
+
+        //Func<string>型の BBを定義し
+        //()内の引数を元に(今回の場合は引数なし)
+        //=>{}内に記載されているメソッドを実行している。
+        };
+
         public Form1()
         {
             InitializeComponent();
-
-            Func<int , string> AA = (1 , AA)=> { string GM = "おはよう" };
-            Func<int , string> BB = (2 , BB)=> { string GE = "こんばんは" };
-            FuncAll(1,AA);
-            FuncAll(2,BB);
-
+            label1.Text = "挨拶1";
+            label2.Text = "挨拶2";
+            label3.Text = "挨拶3";
         }
 
-        public FuncAll(int n , Func<int , string> AA)
+        public void subAA()
         {
-            label1.Text = "こんにちは";
-            label2.Text = AA(1);
-            label3.Text = "こんにちは";
-            label4.Text = "こんにちは";
-            label5.Text = BB(2);//
-            label6.Text = "こんにちは";
+            FuncAll(AA);
         }
-        //AAのときに「おはよう」、BBの個所で「こんばんは」が表示されるようにしたいが出来ない。
-        //詰まる時間がもったいないのできく。
 
-        //上記とは別でふと思い出した疑問点。
-        //そういえば、このゲッターとセッターって詳しく説明できない。
-        //今少し調べた知識としては、ユーザが入力したものをpublicで公開
-        //してしまうと危険。隠蔽するために取得(getしてNo変数にsetしている。)
+        public void subBB()
+        {
+            FuncAll(BB);
+        }
+
+        public void subCC()
+        {
+            string filepathC = "testC.txt";
+            using (StreamWriter CC = new StreamWriter(filepathC))
+            {
+            CC.WriteLine(textBox1);
+            }
+
+        }
+
+        public void subDD()
+        {
+            string filepathD = "testD.txt";
+            //string filepathD = "D:\\testD.txt";
+            //\\をのように書く必要があるので注意。
+            using (StreamReader DD = new StreamReader(filepathD))
+            {
+                textBox1.Text = DD.ReadToEnd();
+            }
+        }
+
+        public void FuncAll(Func<int,string> afunc)
+        {
+            int hogehoge = 12345;
+            label1.Text = "こんにちは";
+            label2.Text = afunc(hogehoge);
+            label3.Text = "こんにちは";
+        }
 
         private void button1_Click(object sender, System.EventArgs e)
         {
-            label1.Text = "共通1";
+            subAA();
+            subDD();
+        }
 
-
+        private void button2_Click(object sender, EventArgs e)
+        {
+            subBB();
+            subCC();
         }
     }
 }
+
+//上記とは別でふと思い出した疑問点。
+//そういえば、このゲッターとセッターって詳しく説明できない。
+//今少し調べた知識としては、ユーザが入力したものをpublicで公開
+//してしまうと危険。隠蔽するために取得(getしてNo変数にsetしている。)
